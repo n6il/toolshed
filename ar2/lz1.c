c7481b3540c0206b4bee55f81ed161aa4e36e1d7
cc <>
1996-07-20 22:24:34 +0000

    Merged in pwz's unixification (Sunos).

--- ar2/lz1.c
+++ ar2/lz1.c
@@ -24,93 +24,24 @@ static char *id = "$Id$";
  *
  *------------------------------------------------------------------
  * $Log$
- * Revision 1.1  1996/07/20 17:10:37  cc
- * Initial revision
+ * Revision 1.2  1996/07/20 22:24:34  cc
+ * Merged in pwz's unixification (Sunos).
  *
+ * Revision 1.1  96/07/20  17:10:37  cc
+ * Initial revision
+ * 
  *------------------------------------------------------------------
  */
 
 #include <stdio.h>
-#include <types.h>
+#ifdef SYSV
+# include <sys/types.h>
+#else 
+# include <types.h>
+#endif
 #include "arerrs.h"
+#include "lz1.h"
 
-#ifndef STATIC
-# define	STATIC	static direct
-#endif
-
-#ifndef WORD
-# ifdef SYSV
-#   define WORD short
-#   define UWORD unsigned short
-#   define VOID void
-# else
-#  ifdef  OSK
-#   define WORD short
-#   define UWORD unsigned short
-#   define VOID void
-#  else
-#   define WORD int
-#   define UWORD unsigned int
-#   define VOID int
-#  endif
-# endif
-#endif
-
-#define BITS		14				/* maximum number of bits/code		*/
-#define OLD_BITS	11				/* old maximum number of bits/code	*/
-#define DEF_BITS	12				/* default number of bits/code		*/
-#define INIT_BITS	9				/* initial number of bits/code		*/
-#define COMP		0				/* we are compressing				*/
-#define DECOMP		1				/* we are decompressing				*/
-
-/*
- * One code can represent 1 << BITS characters, but to get a code of
- * length N requires an input string of at least N * (N - 1) / 2
- * characters. To overflow the decompress stack, an input file would
- * have to have at least MAXSTACK * (MAXSTACK - 1) / 2 consecutive
- * occurrences of a particular character, which is unlikely for the
- * value used here. (Do keep the initial advice in mind, though.)
- */
-#define MAXSTACK	2000			/* size of output stack				*/
-
-#define TAG			2995			/* suggested by M. Meyer			*/
-
-/*
- * The following should be changed to fit your machine and the type
- * you choose for the elements of the array buf.
- * (If you avoid insert_bit() and fetch(), never mind.)
- */
-#define LOG2WSIZE	4				/* log2(size of base type of buf)	*/
-#define WSIZE		16				/* size of base type of buf			*/
-
-#define BytesToBits(b)	((b) << 3)
-#define LowOrder(n)		(~(~0 << (n)))	/* thanks to K & R				*/
-#define HighOrder(n)	(~0 << (n))
-
-typedef struct {
-	UWORD	next,					/* chain of entries with same prefix*/
-			chain,					/* chain prefixed with this entry	*/
-			suffix;					/* last char in this entry			*/
-	} COMPTBL;
-
-typedef struct {
-	UWORD	prefix,					/* prefix code for this entry		*/
-			lastch;					/* last char in this entry			*/
-	} DCOMPTBL;
-
-STATIC WORD		maxbits,			/* user settable max # bits/code	*/
-				n_bits,				/* initial number of bits/code		*/
-				maxmaxcode,			/* max permissible maxcode value	*/
-									/* (i.e. 2 ** BITS - 1)				*/
-				maxcode,			/* 2 ** n_bits - 1					*/
-				free_ent,			/* first unused entry				*/
-				offset;				/* cursor into buf (units of bits)	*/
-
-STATIC long		lz_bytes;
-STATIC UWORD	buf[BITS];
-/*STATIC*/ COMPTBL	*CompTbl;
-/*STATIC*/ DCOMPTBL	*CrakTbl;
-extern int	debug;
 /*page*/
 /*
  *      Writes compressed file to outfile.
@@ -458,11 +389,12 @@ FILE	*fp;
 		else
 			*bp++ = word;
 
+	/* note that we use 'lim' here to get a signed variable */
 	if (cnt & 1)
-		if ((word = getc(fp)) == EOF)
+		if ((lim = getc(fp)) == EOF)
 			return (EOF);
 		else
-			*bp = (word << 8);
+			*bp = (lim << 8);
 #endif
 
 	return (0);
