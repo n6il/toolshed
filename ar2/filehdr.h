e8e021f5500ed8b83b9844e8b00f8c76d2e8c3e7
cc <>
1996-07-20 22:35:53 +0000

    Merged in pwz's unixification (Sunos).

--- ar2/filehdr.h
+++ ar2/filehdr.h
@@ -24,12 +24,17 @@ static char *id4 = "$Id$";
  *
  *------------------------------------------------------------------
  * $Log$
- * Revision 1.1  1996/07/20 17:10:42  cc
- * Initial revision
+ * Revision 1.2  1996/07/20 22:35:53  cc
+ * Merged in pwz's unixification (Sunos).
  *
+ * Revision 1.1  96/07/20  17:10:42  cc
+ * Initial revision
+ * 
  *------------------------------------------------------------------
  */
 
 
 #define OS9MAGIC	0x87CD
 #define OSKMAGIC	0x4AFC
+#define DOSMAGIC	0x4d5a
+#define SUN4MAGIC	0x8103
