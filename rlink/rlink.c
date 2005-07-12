81f38d8ade1e56eb5ee96197e94a15590a606c43
Boisy Pitre <coco@toughmac.com>
2005-07-12 22:51:31 +0000

    More and more...

--- rlink/rlink.c
+++ rlink/rlink.c
@@ -24,14 +24,21 @@ char **argv;
 {
 	char *rfiles[MAX_RFILES];
 	char *lfiles[MAX_RFILES];
-	char *ofile;
+	char *ofile, *modname;
+	int edition, extramem;
+	int printmap, printsym;
 	int rfile_count, lfile_count, i;
 
 
+	modname = NULL;
 	ofile = NULL;
+	edition = 1;
+	extramem = 0;
+	printmap = 0;
+	printsym = 0;
+
 	rfile_count = 0;
 	lfile_count = 0;
-
 
 	/* Parse options */
 
@@ -40,8 +47,64 @@ char **argv;
 		if (argv[i][0] == '-')
 		{
 			/* option -- process it */
-			switch (tolower(argv[i][1]))
+			switch (argv[i][1])
 			{
+				case 'E':
+					/* Edition */
+					{
+						char *p = &argv[i][2];
+
+						if (argv[i][2] == '=')
+						{
+							p++;
+						}
+
+						edition = atoi(p);
+					}
+					break;
+
+				case 'M':
+					/* Extra Memory */
+					{
+						char *p = &argv[i][2];
+
+						if (argv[i][2] == '=')
+						{
+							p++;
+						}
+
+						extramem = atoi(p);
+					}
+					break;
+
+				case 'm':
+					/* Print linkage map */
+					{
+						printmap = 1;
+					}
+					break;
+
+				case 's':
+					/* Print symbol table */
+					{
+						printsym = 1;
+					}
+					break;
+
+				case 'n':
+					/* Name */
+					{
+						char *p = &argv[i][2];
+
+						if (argv[i][2] == '=')
+						{
+							p++;
+						}
+
+						modname = p;
+					}
+					break;
+
 				case 'o':
 					/* Output object file */
 					{
@@ -102,7 +165,7 @@ char **argv;
 
 	/* Call the function which does all the work! */
 
-	return link(rfiles, rfile_count, lfiles, lfile_count, ofile);
+	return link(rfiles, rfile_count, lfiles, lfile_count, ofile, modname, edition, extramem, printmap, printsym);
 }
 
 
@@ -111,24 +174,37 @@ int help()
 {
 	fprintf(stderr, "Usage: rlink <opts>\n");
 	fprintf(stderr, "Options:\n");
-	fprintf(stderr, "   -l[=]libfile   library\n");
+	fprintf(stderr, "   -o[=]file      output object file\n");
+	fprintf(stderr, "   -n[=]name      module name of the object file\n");
+	fprintf(stderr, "   -l[=]path      additional library\n");
+	fprintf(stderr, "   -E[=]edition   edition number in the module\n");
+	fprintf(stderr, "   -M[=]size      additional number of pages of memory\n");
+	fprintf(stderr, "   -m             print the linkage map\n");
+	fprintf(stderr, "   -s             print the symbol table\n");
+/*	fprintf(stderr, "   -b=ept         Make callable from BASIC09\n"); */
+/*	fprintf(stderr, "   -t             allow static data to appear in BASIC09 module\n"); */
 
 	return 0;
 }
 
 
 
-int link(rfiles, rfile_count, lfiles, lfile_count, ofile)
+int link(rfiles, rfile_count, lfiles, lfile_count, ofile, modname, edition, extramem, printmap, printsym)
 char *rfiles[];
 int rfile_count;
 char *lfiles[];
 int lfile_count;
 char *ofile;
+char *modname;
+int edition, extramem, printmap, printsym;
 {
 	int	i;
 
 
 #ifdef DEBUG
+	printf("edition = %d, memsize = %d, modname = %s, printmap=%d, printsym=%d\n",
+		edition, extramem, modname, printmap, printsym);
+
 	if (rfile_count > 0)
 	{
 		printf("ROF files: ");
@@ -175,13 +251,12 @@ char *ofile;
 #ifdef DEBUG
 			dump_rof_header(hd);
 #endif
+			fclose(fp);
 		}
 		else
 		{
 			fprintf(stderr, "linker error: cannot open file %s\n", rfiles[i]);
 		}
-
-		fclose(fp);
 	}
 	
 	return 0;
