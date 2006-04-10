14bdde455de91c48594e059f7f83f9f42ed92c3d
Boisy Pitre <coco@toughmac.com>
2006-04-10 15:24:27 +0000

    rdump now standalone

--- os9/os9.c
+++ os9/os9.c
@@ -57,7 +57,6 @@ static struct cmdtbl table[] =
     {os9makdir,	"makdir"},
     {os9modbust,"modbust"},
     {os9padrom,	"padrom"},
-    {os9rdump,	"rdump"},
     {os9rename,	"rename"},
     {NULL,	NULL}
 };
