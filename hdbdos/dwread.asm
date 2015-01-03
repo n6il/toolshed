36461f0b5a8dda7c66bbedfc0cacf108bb31af46
David Ladd <davidwladd@gmail.com>
2015-01-02 23:57:19 -0600

    Moved becker routine with timeout to a new driver called BECKERTO and restored original BECKER routine from backup.
    Reasoning for this is on slower DW servers the return packets may not come back in time and would generate timeouts often.  This prevented a few people being able to actually use the becker interface on those slower DW servers.
    
    Updated makefile to create new becker ROM’s, BIN’s, WAV’s, & CAS files for the timeout version of those routines.

--- hdbdos/dwread.asm
+++ hdbdos/dwread.asm
@@ -68,6 +68,35 @@ loop@     ldb    $FF4C
           puls      cc,dp,x,u,pc        ; restore registers and return
           ELSE
           IFNE BECKER
+* NOTE: There is no timeout currently on here...
+DWRead    clra                          ; clear Carry (no framing error)
+          deca                          ; clear Z flag, A = timeout msb ($ff)
+          tfr       cc,b
+          pshs      u,x,dp,b,a          ; preserve registers, push timeout msb
+          leau   ,x
+          ldx    #$0000
+          IFEQ   NOINTMASK
+          orcc   #IntMasks
+          ENDC
+loop@     ldb    $FF41
+          bitb   #$02
+          beq    loop@
+          ldb    $FF42
+          stb    ,u+
+          abx
+          leay   ,-y
+          bne    loop@
+          tfr    x,y
+          ldb    #0
+          lda    #3
+          leas      1,s                 ; remove timeout msb from stack
+          inca                          ; A = status to be returned in C and Z
+          ora       ,s                  ; place status information into the..
+          sta       ,s                  ; ..C and Z bits of the preserved CC
+          leay      ,x                  ; return checksum in Y
+          puls      cc,dp,x,u,pc        ; restore registers and return
+          ELSE
+          IFNE BECKERTO
 * NOTE: There is now timeout ...
 DWRead	  clra                  ; clear Carry, Set Z
 	  pshs	 cc,x,u         ; save regs
@@ -102,8 +131,9 @@ rdy@      puls	 x		; restore X
           ENDC
           ENDC
           ENDC
+          ENDC
 
-          IFEQ BECKER+JMCPBCK+ARDUINO
+          IFEQ BECKER+JMCPBCK+ARDUINO+BECKERTO
           IFNE BAUD38400
 *******************************************************
 * 38400 bps using 6809 code and timimg
