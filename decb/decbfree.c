6cdcaaa88db0c3ecadd5a1c3be000bad81362d7a
Tim Lindner <tlindner@macmess.org>
2015-02-01 11:49:57 -0800

    Removed unused function from decbfree.c

--- decb/decbfree.c
+++ decb/decbfree.c
@@ -13,8 +13,6 @@
 #include <toolshed.h>
 
 
-static int do_free(char **argv, char *p);
-
 /* Help message */
 static char const * const helpMessage[] =
 {
@@ -86,67 +84,5 @@ int decbfree(int argc, char *argv[])
 		return(0);
 	}
 
-	return(0);
-}
-
-
-static int do_free(char **argv, char *p)
-{
-	error_code	ec = 0;
-	int i;
-	char decbpathlist[256];
-	decb_path_id path;
-	int free_granules = 0;
-
-
-	/* 1. Make a copy of the pathlist. */
-	
-	strcpy(decbpathlist, p);
-
-
-	/* 2. If a comma isn't present int he string, then add it so that the path is opened as a non-native path. */
-	
-	if (strchr(decbpathlist, ',') == NULL)
-	{
-		strcat(decbpathlist, ",");
-	}
-	
-	
-	/* 3. Open a path to the device. */
-	
-	ec = _decb_open(&path, decbpathlist, FAM_READ);
-
-	if (ec != 0)
-	{
-		fprintf(stderr, "%s: error %d opening '%s'\n",
-			argv[0], ec, decbpathlist);
-
-		return(ec);
-	}
-
-
-	/* 4. Walk the FAT. */
-
-	for (i = 0; i < 256; i++)
-	{
-		if (path->FAT[i] == 0xFF)
-		{
-			free_granules++;
-		}
-	}
-
-
-	/* 5. Report the free space. */
-	
-	printf("Free granules: %d (%d bytes)\n", free_granules, free_granules * (4608 / 2));
-
-
-	/* 6. Close the path. */
-	
-	_decb_close(path);
-
-
-	/* 7. Return status. */
-	
 	return(0);
 }
